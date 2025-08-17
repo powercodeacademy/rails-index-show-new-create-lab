0ad04beebac427c9779110c0c64241e255fecb78
Jen Kelly <jen.kelly@powerhrg.com>
2025-08-17 14:25:45 -0400

    Refactor coupon display method

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,6 +2,6 @@ class Coupon < ApplicationRecord
   validates :coupon_code, :store, presence: true
 
   def display
-    self.store + ": " + self.coupon_code
+    self.coupon_code + " | " + self.store
   end
 end
