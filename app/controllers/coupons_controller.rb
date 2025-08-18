f7f1b8233d62ba7eee0987a2689da39ac3605891
Emily Kanarek <emilyakanarek@gmail.com>
2025-08-18 11:39:58 -0400

    Redirect from form to coupon page

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -4,13 +4,20 @@ class CouponsController < ApplicationController
   end
 
   def show
+    @coupon = Coupon.find_by(id: params[:id])
   end
 
   def new
   end
 
   def create 
-    @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+    @coupon = Coupon.create(coupon_params)
     redirect_to coupon_path(@coupon)
+  end
+
+  private 
+
+  def coupon_params 
+    params.require(:coupon).permit(:coupon_code, :store) 
   end
 end
